3d80abe5fa7c39c3005c332c07f9081fe86b4705
Priya Darshini <priyakinnu18@gmail.com>
2026-09-24 23:18:48 +0530

    Configure Express backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -30,5 +30,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong on the server!' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
